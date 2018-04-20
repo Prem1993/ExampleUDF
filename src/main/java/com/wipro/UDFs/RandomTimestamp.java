575a2eeda63edf601d43283cdb57e5ed9992add8
Prem Kumar <prem.kumar51@wipro.com>
2018-04-19 20:40:27 -0700

    param updated

--- src/main/java/com/wipro/UDFs/RandomTimestamp.java
+++ src/main/java/com/wipro/UDFs/RandomTimestamp.java
@@ -19,11 +19,11 @@ public class RandomTimestamp extends UDF {
     // Accept a string input
     public Timestamp evaluate(String input) {
         // If the value is null, return a null
-        if(input == null)
+        if(input == null || input.equalsIgnoreCase("null"))
             return null;
 
-        long offset = Timestamp.valueOf("1990-01-01 00:00:00").getTime();
-        long end = Timestamp.valueOf("2013-01-01 00:00:00").getTime();
+        long offset = Timestamp.valueOf("1920-01-01 00:00:00").getTime();
+        long end = Timestamp.valueOf("1980-01-01 00:00:00").getTime();
         long diff = end - offset + 1;
         Timestamp rand = new Timestamp(offset + (long)(Math.random() * diff));
 
